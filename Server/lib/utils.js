9c7daf41d23a3c1abf45bdb9c36b9a6ee7b89dd2
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-16 14:37:14 +0530

    Utilities

--- Server/lib/utils.js
+++ Server/lib/utils.js
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken';
 
 // Function to generate token for user
 export const generateToken = (userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET);
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
     return token;
 }
