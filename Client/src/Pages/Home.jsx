a7cd8b68d04f521d13b776cbd49d7f72569ec9d9
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-16 14:27:11 +0530

    Home Page

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from '../MyComponents/Sidebar'
 import ChatContainer from '../MyComponents/ChatContainer'
 import RightSidebar from '../MyComponents/RightSidebar'
+import { ChatContext } from '../../Context/ChatContext'
 
 const Home = () => {
 
-    const [selectedUser, setSelectedUser] = React.useState(false)
+    const {selectedUser} = useContext(ChatContext);
     return (
       <div>
           <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
               <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'grid-cols-2'}`}>
                   <Sidebar />
 
-                  <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+                  <ChatContainer />
 
-                  <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+                  <RightSidebar />
               </div>
           </div>
       </div>
